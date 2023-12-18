b2270104ff57eda066a3d441de0cb383b090a624
Gajanan Vijay <Gajanan Vijay@TYSS-Gajanan-Vijay>
2023-12-18 12:12:35 +0530

    Fork class generated...

---
+++ PROJECT2/src/test/java/companies/Demo.java
@@ -0,0 +1,10 @@
+package companies;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Fork class generated...");
+	}
+
+}
